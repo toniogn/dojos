0a210ec4ef2c2240f3802232a78f4369bb0000b9
toniogn <85986987+toniogn@users.noreply.github.com>
2021-11-02 15:06:59 +0100

    Update clean_the_grid.py

--- clean_the_grid.py
+++ clean_the_grid.py
@@ -2,14 +2,39 @@ import numpy as np
 from typing import List, Tuple
 
 class Square:
+    """A square of the grid.
+    
+    Parameters
+    ----------
+    x : int
+        Horizontal index of the square on the grid beginning at 0: left.
+    y : int
+        Vertical index of the square on the grid beginning at 0: top.
+    occupied : bool
+        Rather the square is occupied or not.
+        
+    Attributes
+    ----------
+    cleaned : bool
+        Rather the square is clean or not.
+    tetas : List[float]
+        Angles of the direction of the robot each time it passes on the square.
+    """
     def __init__(self, x: int, y: int, occupied: bool = False) -> None:
         self.x = x
         self.y = y
         self.cleaned = False
         self.occupied = occupied
-        self.tetas = []
+        self.tetas: List[float] = []
 
-    def clean(self, teta: float):
+    def clean(self, teta: float) -> None:
+        """Cleans the square and append the robot's direction.
+        
+        Parameters
+        ----------
+        teta : float
+            Angle of the direction of the robot.
+        """
         self.cleaned = True
         self.tetas.append(teta)
 
@@ -17,6 +42,22 @@ class Square:
         return f"Square({self.x}, {self.y}, {self.occupied})"
 
 class Robot:
+    """Cleaning robot.
+    
+    Parameters
+    ----------
+    grid : List[str]
+        List of grid's lines as a string with '.' for empty square and 'x' for occupied square.
+    
+    Attributes
+    ----------
+    squares : np.array[Square]
+        A numpy 2D-array representing the grid's squares.
+    square : Square
+        The current square where the robot is.
+    teta : float
+        The current direction's angle of the robot (0: looking to the right, pi/2: looking to the bottom, etc...).
+    """
     def __init__(self, grid: List[str]) -> None:
         self.squares = np.array([[Square(x, y, char == "x") for x, char in enumerate(row)] for y, row in enumerate(grid)])
         self.square = self.squares[0, 0]
