91aa61bd1514316685516f0141064aade2af27c4
toniogn <85986987+toniogn@users.noreply.github.com>
2021-11-02 23:17:50 +0100

    Update avoid_credit.py

--- avoid_credit.py
+++ avoid_credit.py
@@ -13,28 +13,31 @@ def minimum_relocations(operations: List[int]) -> int:
     relocations_number : int
         Minimum number of expanses to relocate.
     """
-    expanses = {
-        str(index_expanse): value_expanse
-        for index_expanse, value_expanse in enumerate(operations)
-        if value_expanse < 0
-    }
     relocations_number = 0
     index = 0
     while index < len(operations):
         if sum(operations[: index + 1]) < 0 and any(value > 0 for value in operations[index + 1 :]):
-            operations.append(
-                operations.pop(
-                    min(
-                        (
-                            int(index_expanse)
-                            for index_expanse in expanses.keys()
-                            if int(index_expanse) <= index
-                        ),
-                        key=lambda index_expanse: expanses[str(index_expanse)],
-                    )
-                )
-            )
+            relocate(operations, index)
             relocations_number += 1
         else:
             index += 1
     return relocations_number
+
+def relocate(operations: List[int], index: int) -> None:
+    """Relocate the biggest expanse at the end of the operations list.
+    
+    Parameters
+    ----------
+    operations : List[int]
+        Chronological list of operations.
+    index : int
+        Current index where the accumulated operations leads to a negative amount.
+    """
+    operations.append(
+        operations.pop(
+            min(
+                range(index + 1),
+                key=lambda i: operations[i],
+            )
+        )
+    )
