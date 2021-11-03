e5d38206294ce9e4b47af29ae4b9f53d3add84da
toniogn <85986987+toniogn@users.noreply.github.com>
2021-11-03 16:45:41 +0100

    Update avoid_credit_template.py

--- avoid_credit_template.py
+++ avoid_credit_template.py
@@ -1,14 +1,14 @@
-import json
-import unittest
+import numpy as np
+from typing import List
 
 def minimum_relocations(operations: List[int]) -> int:
   pass
 
-
-class TestMinimumRelocations(unittest.TestCase):
-  def 
-  
-  def test_a(self):
-    operations = ()
-    minimum_relocations = 
+def operations_generator(operations_number: int) -> List[int]:
+  assert operations_number % 2 != 0
+  return (round(value) for value in 100 * np.cos([k * 2 * np.pi/(operations_number - 1) for k in range(operations_number)]))
+    
+if __name__ == "__main__":
+    for operations_number, minimum_relocations_number in [(int(1e3 + 1), 167)]:
+      assert minimum_relocations(list(operations_generator(operations_number))) == minimum_relocations_number
 
