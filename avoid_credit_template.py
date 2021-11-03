5bb4f1cf3946d887135ed0b8ed9182796eb2def5
toniogn <85986987+toniogn@users.noreply.github.com>
2021-11-03 14:12:39 +0100

    Create avoid_credit_template.py

---
+++ avoid_credit_template.py
@@ -0,0 +1,14 @@
+import json
+import unittest
+
+def minimum_relocations(operations: List[int]) -> int:
+  pass
+
+
+class TestMinimumRelocations(unittest.TestCase):
+  def 
+  
+  def test_a(self):
+    operations = ()
+    minimum_relocations = 
+
