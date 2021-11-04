d993f496572f086afaa235d90c962e1d167e1f4f
toniogn <85986987+toniogn@users.noreply.github.com>
2021-11-04 14:48:33 +0100

    Create climb_the_lighthouse_template.py

---
+++ climb_the_lighthouse_template.py
@@ -0,0 +1,18 @@
+def stair_path_number(stair_steps_number: int) -> int:
+    """Counts the number of different path to climb the stairway depending on its steps number.
+
+    Parameters
+    ----------
+    stair_steps_number : int
+        Number of steps of the stair.
+
+    Returns
+    -------
+    int
+        Number of different path to climb the stairway.
+    """
+    pass
+  
+  
+  if __name__ == "__main__":
+    assert stair_path_number(35) == 14930352
